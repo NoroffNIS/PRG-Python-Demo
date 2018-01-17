993125588739dde1dfee0feec10acfc483df75bf
rbrage <brage89@gmail.com>
2018-01-17 22:44:10 +0100

    time example

---
+++ 11-Time_RE.py/datetime_today().py
@@ -0,0 +1,5 @@
+import datetime
+import pytz #python -m pip install pytz
+
+#dt = datetime.datetime(2018,7,13,9,30,5000, tzinfo=pytz.UTC)
+#dt_now = datetime.datetime.now(2018,7,13,9,30,5000, tz=pytz.UTC)
