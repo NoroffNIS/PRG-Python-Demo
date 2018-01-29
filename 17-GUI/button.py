4aadc70a5a35e4b13f6cbd665f421b770970f147
Brage Roalkvam <brage89@gmail.com>
2018-01-29 11:24:56 +0100

    17 example

---
+++ 17-GUI/button.py
@@ -0,0 +1,30 @@
+from tkinter import *
+root = Tk()
+root.title('First window')
+counter = 0
+vtext = StringVar()
+vtext.set('Hello world!')
+
+def switch_bg():
+    if root.cget('bg') == 'red':
+        root.configure(bg = 'gray')
+    else:
+        root.configure(bg = 'red')
+
+def count():
+    global counter
+    counter += 1
+    vtext.set(str(counter))
+
+label = Label(root, textvariable=vtext)
+btn_switch = Button(root, text='Switch!', command=switch_bg)
+btn_count = Button(root, text='Count!', command=count)
+btn_exit = Button(root, text='Exit!', command=exit)
+
+label.pack(padx = 100, pady = 20)
+btn_switch.pack(padx = 100, pady=40)
+btn_switch.pack(padx = 50, pady=60)
+btn_count.pack(padx = 75, pady=60)
+btn_exit.pack(padx = 100, pady=60)
+
+root.mainloop()
---
+++ 17-GUI/hello_world.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+root = Tk()
+root.title('First window')
+label = Label(root, text='Hello world!')
+label.pack(padx = 200, pady = 50)
+root.mainloop()
