367dc0ef5d7f182b9354c362c91b9a3697bedfd9
Brage Roalkvam <brage89@gmail.com>
2018-01-30 08:21:40 +0100

    18 - examples

---
+++ 18-Socket/server.py
@@ -0,0 +1,23 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (socket.gethostname() , 50000)
+s.bind(server_address)
+s.listen(1)
+print('Listning on {}:{}'.format(*server_address))
+while True:
+    conn, client_addr = s.accept()
+    try:
+        print('Connection from {}'.format(client_addr))
+        while True:
+            data = conn.recv(1024)
+            print('Recived: {!r}'.format(data))
+            if data:
+                print('sending data back to {}'.format(client_addr))
+                conn.sendall(data)
+            else:
+                print('no data from {}'.format(client_addr))
+                break
+    finally:
+        print('Closing socket')
+        conn.close()
+        break
